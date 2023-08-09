01a650fd44b56f6a0a2cb929bda5081f1c8181d2
Remy Clarke <60509493+ClarkeRemy@users.noreply.github.com>
2023-08-09 23:31:26 +0900

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:futures-util/src/sink/into_feed.rs

--- futures-util/src/sink/into_feed.rs
+++ futures-util/src/sink/into_feed.rs
@@ -1,3 +1,4 @@
+use core::marker::PhantomData;
 use core::pin::Pin;
 use futures_core::future::Future;
 use futures_core::ready;
@@ -14,41 +15,43 @@ pub struct IntoFeed<Si: ?Sized, Item> {
 
 fn playground() {
     struct G<T>(T);
+    struct H<'a>(&'a i32);
+
 
     type Gn = G<i32>;
 
     fn take_unpin<T: Unpin>(unpin: T) {}
-
+    let a = &mut 5;
     take_unpin(G(1));
-    take_unpin(G(Pin::new()));
+    take_unpin(G(async{ *a + 4}));
 }
 
-// Pinning is never projected to children
-impl<Si: Unpin + ?Sized, Item> Unpin for IntoFeed<Si, Item> {}
+// // Pinning is never projected to children
+// impl<Si: Unpin + ?Sized, Item> Unpin for IntoFeed<Si, Item> {}
 
-impl<'a, Si: Sink<Item> + Unpin + ?Sized, Item> Feed<'a, Si, Item> {
-    pub(super) fn new(sink: &'a mut Si, item: Item) -> Self {
-        Feed { sink, item: Some(item) }
-    }
+// impl<'a, Si: Sink<Item> + Unpin + ?Sized, Item> Feed<'a, Si, Item> {
+//     pub(super) fn new(sink: &'a mut Si, item: Item) -> Self {
+//         Feed { sink, item: Some(item) }
+//     }
 
-    pub(super) fn sink_pin_mut(&mut self) -> Pin<&mut Si> {
-        Pin::new(self.sink)
-    }
+//     pub(super) fn sink_pin_mut(&mut self) -> Pin<&mut Si> {
+//         Pin::new(self.sink)
+//     }
 
-    pub(super) fn is_item_pending(&self) -> bool {
-        self.item.is_some()
-    }
-}
+//     pub(super) fn is_item_pending(&self) -> bool {
+//         self.item.is_some()
+//     }
+// }
 
-impl<Si: Sink<Item> + Unpin + ?Sized, Item> Future for Feed<'_, Si, Item> {
-    type Output = Result<(), Si::Error>;
+// impl<Si: Sink<Item> + Unpin + ?Sized, Item> Future for Feed<'_, Si, Item> {
+//     type Output = Result<(), Si::Error>;
 
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let this = self.get_mut();
-        let mut sink = Pin::new(&mut this.sink);
-        ready!(sink.as_mut().poll_ready(cx))?;
-        let item = this.item.take().expect("polled Feed after completion");
-        sink.as_mut().start_send(item)?;
-        Poll::Ready(Ok(()))
-    }
-}
+//     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+//         let this = self.get_mut();
+//         let mut sink = Pin::new(&mut this.sink);
+//         ready!(sink.as_mut().poll_ready(cx))?;
+//         let item = this.item.take().expect("polled Feed after completion");
+//         sink.as_mut().start_send(item)?;
+//         Poll::Ready(Ok(()))
+//     }
+// }
